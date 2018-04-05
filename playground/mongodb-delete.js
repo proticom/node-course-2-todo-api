86f0ffc6d2e0b7b86a44b8de15559044be5f6a57
Edward Tadros <edward@Edwards-MBP.frontiernet.net>
2018-04-05 06:49:04 -0700

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,47 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+// deleteMany
+// db.collection('Todos').deleteMany({text: 'Eat Lunch'}).then((result) => {
+//   console.log(result);
+// });
+
+// deleteOne
+// db.collection('Todos').deleteOne({text: 'Eat Lunch'}).then((result) => {
+//   console.log(result);
+// });
+
+//findOneAndDelete
+// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+//   console.log(result);
+// });
+
+// deleteMany
+// db.collection('Users').deleteMany({name: 'Edward'}).then((result) => {
+//   console.log(result);
+// });
+
+//findOneAndDelete
+db.collection('Users').findOneAndDelete({
+  _id : new ObjectID('5ac395bbfaad8b394f700a97')
+}).then((result) => {
+  console.log(result);
+});
+
+// db.collection('Users').find({
+//   _id: new ObjectID('5ac395bbfaad8b394f700a97')
+// }).toArray().then((docs) => {
+//   console.log('Users');
+//   console.log(JSON.stringify(docs, undefined, 2));
+// }, (err) => {
+//   console.log('Unable to fetch users', err);
+// });
+
+  //db.close();
+});
