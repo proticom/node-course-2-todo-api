3e9db42849cc17fef0da841b89143d95206cf000
Edward Tadros <edward@Edwards-MBP.frontiernet.net>
2018-04-09 11:04:56 -0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -19,8 +19,16 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
-
 });
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(3033, () => {
   console.log('Started on port 3033');
