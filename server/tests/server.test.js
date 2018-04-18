ccb83d568cd2b2742f02bdab0728654e6aaf92a1
Edward Tadros <edward@Edwards-MBP.frontiernet.net>
2018-04-17 18:33:16 -0700

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -133,5 +135,93 @@ describe('DELETE /todos/:id', () => {
       .delete('/todos/123abc')
       .expect(404)
       .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    // grab id of first item
+    // update text, st completed = true
+    // assert 200
+    // custom assert text is changed, completed is true, completedAt is a number .toBeA
+    var hexId = todos[0]._id.toHexString();
+    var text = 'Updated by test';
+
+    // HOW I DID IT
+    // request(app)
+    //   .patch(`/todos/${hexId}`)
+    //   .send({
+    //     text: text,
+    //     completed: true
+    //   })
+    //   .expect(200)
+    //   .end((err, res) => {
+    //     if (err) {
+    //       return done(err);
+    //     }
+    //     Todo.findById(hexId).then((todo) => {
+    //         expect(todo.text).toBe(text);
+    //         expect(todo.completed).toBe(true);
+    //         expect(todo.completedAt).toBeA('number');
+    //         done();
+    //       }).catch((e) => done(e));
+    //     });
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text: text,
+        completed: true
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    // grab id of second todo item
+    // update text, set completed to false
+    // assert 200
+    // assert text is changed, completed is false, completedAt is null .toNotExist
+    var hexId = todos[1]._id.toHexString();
+    var text = 'Updated by test !!';
+
+    // HOW I DID IT
+    // request(app)
+    //   .patch(`/todos/${hexId}`)
+    //   .send({
+    //     text: text,
+    //     completed: false
+    //   })
+    //   .expect(200)
+    //   .end((err, res) => {
+    //     if (err) {
+    //       return done(err);
+    //     }
+    //     Todo.findById(hexId).then((todo) => {
+    //         expect(todo.text).toBe(text);
+    //         expect(todo.completed).toBe(false);
+    //         expect(todo.completedAt).toNotExist();
+    //         done();
+    //       }).catch((e) => done(e));
+    //     });
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text: text,
+        completed: false
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+
   });
 });
