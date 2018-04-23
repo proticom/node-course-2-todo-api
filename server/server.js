edf6b4d3d2f7c41dc55d218b743c1b2afe56c3c3
Edward Tadros <edward@Edwards-MBP.frontiernet.net>
2018-04-23 14:25:07 -0700

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -108,7 +108,6 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
-// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email','password']);
   var user = new User(body);
@@ -126,7 +125,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
