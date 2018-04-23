9618ac4fe4366f4728ad0b3ad360ee26febaf7d1
Edward Tadros <edward@Edwards-MBP.frontiernet.net>
2018-04-23 13:59:48 -0700

    Add POST /user/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
+const{SHA256} = require('crypto-js');
 
 var {mongoose} = require('./db/mongoose');
 var{Todo} = require('./models/todo');
@@ -122,6 +124,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
